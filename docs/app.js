e659e77293988d04fdbd98ae913947be2dcc792e
Avraham Essoudry <essoudry@gmail.com>
2019-01-13 20:10:50 +0200

    work in progress

--- docs/app.js
+++
@@ -1,8 +0,0 @@
-export class App
-{
-    constructor()
-    {
-        this.directions = ["a1-ltr", "a1-rtl"];
-        this.selectedDirection = "a1-ltr";
-    }
-}
---
+++ docs/docs.js
@@ -0,0 +1,6 @@
+function directionChanged()
+{
+    var classes = document.querySelector("main.demo-direction").classList;
+    classes.toggle("a1-ltr");
+    classes.toggle("a1-rtl");
+}
